b2bde37f38e2480b8c90a230e44000d1c64c28e3
Le Viet-Hoang <hashdotlee@gmail.com>
2023-11-27 05:19:59 +0700

    feat: add more variables to tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,35 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        "slide-out": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.2s ease-out forwards",
+        "slide-out": "slide-out 0.2s ease-out forwards",
+        "fade-in": "fade-in 0.2s ease-out forwards",
+        "fade-out": "fade-out 0.2s ease-out forwards",
+      },
+      spacing: {
+        sidebar: "var(--sidebar-width)",
+        "sidebar-mobile": "var(--sidebar-mobile-width)",
+        header: "var(--header-height)",
+      },
     },
   },
   plugins: [],
