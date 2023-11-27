de746a313da06a94290f947aec4a4ce572b23d48
Le Viet-Hoang <hashdotlee@gmail.com>
2023-11-27 18:38:40 +0700

    improve readability for pagination

--- app/(home)/upcoming/page.tsx
+++ app/(home)/upcoming/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function Upcoming() {
   return (
-    <div className="px-4 mt-header dark:bg-zinc-900">
+    <div className="px-4 py-8 mt-header dark:bg-zinc-900">
       <UpcomingMovieList />
     </div>
   );
--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import ReactPaginate from "react-paginate";
 
 interface PaginationProps {
@@ -13,8 +14,8 @@ export default function Pagination({
 }: PaginationProps) {
   return (
     <ReactPaginate
-      previousLabel={"Previous"}
-      nextLabel={"Next"}
+      previousLabel={<ChevronLeftIcon className="h-5 w-5" />}
+      nextLabel={<ChevronRightIcon className="h-5 w-5" />}
       initialPage={page - 1}
       pageRangeDisplayed={2}
       marginPagesDisplayed={1}
@@ -23,25 +24,27 @@ export default function Pagination({
         onPageChange(data);
       }}
       containerClassName={
-        "flex items-center mx-auto flex-wrap flex justify-center gap-2"
+        "flex items-center mx-auto py-4 flex-wrap flex justify-center gap-2"
       }
       previousLinkClassName={
-        "px-4 rounded-md text-sm py-2 truncate dark:bg-zinc-800 dark:text-zinc-200"
+        "px-4 rounded-md inline-block text-sm py-2 truncate dark:bg-zinc-800 dark:text-zinc-200"
       }
-      breakClassName={
+      previousClassName="flex items-center"
+      nextClassName="flex items-center"
+      breakLinkClassName={
         "px-4 rounded-md py-2 dark:bg-zinc-800 dark:text-zinc-200"
       }
       nextLinkClassName={
-        "py-2 px-4 rounded-md text-sm truncate dark:bg-zinc-800 dark:border dark:border-zinc-800 dark:text-zinc-200"
+        "px-4 rounded-md inline-block text-sm py-2 truncate dark:bg-zinc-800 dark:text-zinc-200"
       }
-      disabledClassName={
-        "px-4 rounded-md py-2 dark:bg-zinc-900 dark:text-zinc-200"
+      disabledLinkClassName={
+        "px-4 rounded-md py-2 dark:text-zinc-600 dark:bg-zinc-900 dark:text-zinc-200"
       }
-      pageClassName={
+      pageLinkClassName={
         "py-2 px-4 rounded-md text-sm dark:bg-zinc-800 dark:border dark:border-zinc-800 dark:text-zinc-200"
       }
-      activeClassName={
-        "py-2 px-4 rounded-md dark:bg-zinc-600 dark:text-zinc-200"
+      activeLinkClassName={
+        "py-2 px-4 rounded-md dark:bg-zinc-200 dark:text-zinc-900"
       }
     />
   );
