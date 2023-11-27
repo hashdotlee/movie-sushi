94b62d0ef7d830119dd135f0beeb8185284731de
Le Viet-Hoang <hashdotlee@gmail.com>
2023-11-27 08:41:27 +0700

    fix: movie detail

---
+++ app/(home)/detail/[id]/page.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import useCredits from "@/api/useCredit";
+import useExternalIds from "@/api/useExternalIds";
+import useKeywords from "@/api/useKeywords";
+import useMovieDetail from "@/api/useMovieDetail";
+import useVideos from "@/api/useVideos";
+import CategoryList from "@/app/components/CategoryList";
+import NoImage from "@/app/components/NoImage";
+import SocialList from "@/app/components/SocialList";
+import getMovieImage from "@/lib/getMovieImage";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+export default function MovieDetail() {
+  const { id } = useParams();
+  const { movie } = useMovieDetail(Number(id));
+  const { credits } = useCredits(Number(id));
+  const { keywords } = useKeywords(Number(id));
+  const { videos } = useVideos(Number(id));
+  const { externalIds } = useExternalIds(Number(id));
+
+  if (!movie) return null;
+  return (
+    <div>
+      <div className="relative max-h-[50vh] overflow-hidden h-screen">
+        {movie.backdrop_path ? (
+          <Image
+            src={
+              getMovieImage(movie?.backdrop_path, "original") ||
+              "/placeholder.png"
+            }
+            alt={movie.title}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto object-cover object-center"
+          />
+        ) : (
+          <NoImage />
+        )}
+        <div className="absolute top-0 z-10 left-0 w-full h-full from-zinc-900/20 to-current bg-gradient-radial"></div>
+      </div>
+      <div className="flex flex-col xl:flex-row lg:gap-8 gap-2 px-16 my-4">
+        <Image
+          src={getMovieImage(movie.poster_path) || "/placeholder.png"}
+          alt={movie.title}
+          width={0}
+          placeholder="blur"
+          blurDataURL={"/placeholder.png"}
+          height={0}
+          sizes="100vw"
+          className="w-48 h-72 z-20 hidden xl:block lg:-translate-y-1/2 object-center rounded-xl aspect-[3/4] object-cover"
+        />
+        <div className="flex flex-col lg:flex-row gap-8 dark:text-zinc-200 w-full">
+          <div className=" max-w-prose flex flex-col gap-4 flex-grow">
+            <div className="">
+              <h1 className="text-3xl font-bold my-3">{movie.title}</h1>
+              <div className="flex gap-1 text-neutral-600 dark:text-zinc-400 items-center text-sm">
+                <div>
+                  {movie.vote_average.toFixed(1)} ⭐ ({movie.vote_count} votes)
+                </div>
+                <span>&bull;</span>
+                <div>
+                  {movie.genres.map((genre, i) => (
+                    <div key={genre.id}>
+                      <span className="text-sm">{genre.name}</span>
+                      {i < movie.genres.length - 1 && <span>, </span>}
+                    </div>
+                  ))}
+                </div>
+                <span>&bull;</span>
+                <div>
+                  <span>{(movie.runtime / 60).toFixed(0)}h</span>
+                  <span>{movie.runtime % 60}m</span>
+                </div>
+              </div>
+              <p className="text-sm my-2">{movie.overview}</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">Cast</h3>
+              <div className="flex gap-2 p-4">
+                {credits?.cast?.slice(0, 5)?.map((cast) => (
+                  <div
+                    key={cast.id}
+                    className="flex w-1/5 text-center flex-col items-center"
+                  >
+                    <Image
+                      src={
+                        getMovieImage(cast.profile_path, "w500") ||
+                        "/default_avatar.svg"
+                      }
+                      width={0}
+                      height={0}
+                      alt={cast.name}
+                      sizes="100vw"
+                      className="object-cover object-center aspect-square w-16 h-16 rounded-full"
+                    />
+                    <div className="text-sm">{cast.name}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="w-full">
+              <h3 className="text-lg font-semibold my-3">Videos</h3>
+              <div className=" overflow-x-scroll">
+                <div className="flex gap-4">
+                  {videos?.results?.map((video) => (
+                    <iframe
+                      key={video.id}
+                      src={`https://www.youtube.com/embed/${video.key}`}
+                      title="YouTube video player"
+                      className="border-0 rounded-2xl w-full h-auto aspect-video shrink-0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen
+                    ></iframe>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3 className="font-semibold my-2">IMDb</h3>
+            <Link
+              href={`https://www.imdb.com/title/${externalIds?.imdb_id}`}
+              className="font-semibold flex items-center gap-2 text-blue-800 dark:text-blue-400"
+            >
+              <Image src="/imdb-icon.svg" width={48} height={48} alt="imdb" />
+            </Link>
+            <div>
+              <h3 className="font-semibold my-2">Budget</h3>
+              <p>
+                {movie.budget.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </p>
+            </div>
+            <div>
+              <h3 className="font-semibold my-2">Release Date</h3>
+              <p>{movie.release_date}</p>
+            </div>
+            <div>
+              <h3 className="font-semibold my-2">Social</h3>
+              <SocialList externalIds={externalIds} />
+            </div>
+            <div>
+              <h3 className="font-semibold my-2">Popularity</h3>
+              {movie.popularity}
+            </div>
+            <div>
+              <h3 className="font-semibold mb-2">Keywords</h3>
+              <CategoryList
+                categories={keywords?.keywords.slice(0, 10).map((item) => ({
+                  ...item,
+                  value: item.name,
+                }))}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
